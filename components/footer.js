cf39133286c1de6d4f05988f9343bfc8c60ff821
mohsen shabanian <mohsen1299@gmail.com>
2019-10-16 21:08:23 +0330

    remove footer and nav

--- components/footer.js
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-    return (
-        <footer >
-            <p >طراحی توسط <a href="http://mohsenshabanian.com">محسن شعبانیان</a> با کمک <a href="https://getbootstrap.com">بوت استرپ</a></p>
-        </footer>
-    )
-}
--- components/websiteLayout.js
+++ components/websiteLayout.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 
-import Nav from './nav';
-import Footer from './footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const fullpage = {
     minHeight: "calc(100vh - 114px)"
@@ -9,20 +8,46 @@ const fullpage = {
 
 const WebsiteLayout = props => (
     <div>
+
         <Head>
             <title>لاگ رامسر</title>
             <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-            <link rel="stylesheet" href="/css/bootstrap.min.css" />
+            {/* <link rel="stylesheet" href="/css/bootstrap.min.css" /> */}
             <link rel="stylesheet" href="/css/style.css" />
         </Head>
 
-        <Nav />
+        <nav className="navbar navbar-expand-lg  sticky-top navbar-dark bg-dark">
+            <Link href='/'><a className="navbar-brand" >
+                <img src="/img/logo.png" width="30" height="30" className="d-inline-block align-top" alt="logo" />
+                لاگ رامسر
+                    </a>
+            </Link>
+
+            <ul className="navbar-nav">
+
+                <li className="nav-item">
+                    <Link href='/'><a className="nav-link">صفحه اصلی</a></Link>
+                </li>
+                <li className="nav-item">
+                    <Link href='/about'><a className="nav-link">درباره ما</a></Link>
+                </li>
+                <li className="nav-item">
+                    <Link href='/lugNews'><a className="nav-link">اخبار لاگ ها</a></Link>
+                </li>
+                <li className="nav-item">
+                    <Link href='/contact'><a className="nav-link">ارتباط با ما</a></Link>
+                </li>
+            </ul>
+
+        </nav>
 
         <div style={fullpage}>
             {props.children}
         </div>
 
-        <Footer />
+        <footer >
+            <p >طراحی توسط <a href="http://mohsenshabanian.com">محسن شعبانیان</a> با کمک <a href="https://getbootstrap.com">بوت استرپ</a></p>
+        </footer>
     </div>
 );
 
